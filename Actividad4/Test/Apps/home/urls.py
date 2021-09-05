d4cf832a3f0227d002e8cefe9d290991a57cdb68
josé <perezerik387@gmail.com>
2021-09-04 18:25:11 -0600

    Actualizando tareas

--- Actividad4/Test/Apps/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import path,include
-from .views import HomeView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-  path('index/', HomeView.as_view(), name='home'),
-]
